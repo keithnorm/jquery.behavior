b89e23fadaef1ded30f8e84ba73ccc3118167dc7
Keith Norman <keithn@groupon.com>
2011-01-16 13:34:46 -0600

    adding access to super method (vis this._super()) when subclassing behaviors

--- src/jquery.behavior.js
+++ src/jquery.behavior.js
@@ -28,7 +28,7 @@
     name = name.split( "." )[ 1 ];
     
     var object = $[ namespace ][ name ];
-    $[ namespace ][ name ] = function(options, element) {
+    var behavior = $[ namespace ][ name ] = function(options, element) {
       if ( arguments.length ) {
         object.call(this, options, element);
         $.each(this._bindings(), $.proxy(function(i, binding){
@@ -41,17 +41,40 @@
       }
     };
 
-    $.extend($[ namespace ][ name ], { instances: [] });
+    var proto = $[ namespace ][ name ].prototype = object.prototype;
 
-    $[ namespace ][ name ].prototype = object.prototype;
+    // add _super method if we're overriding any methods on base class
+    if(base.prototype){
+      for(var method in proto){
+        proto[method] = ($.isFunction(proto[method]) && $.isFunction(base.prototype[method])) ? (function(name, fn){
+          return function() {
+            var tmp = this._super;
+            
+            // Add a new ._super() method that is the same method
+            // but on the super-class
+            this._super = base.prototype[name];
+            
+            // The method only need to be bound temporarily, so we
+            // remove it when we're done executing
+            var ret = fn.apply(this, arguments);        
+            this._super = tmp;
+            
+            return ret;
+          };
+        })(method, proto[ method ]) : proto[ method ];
+      }
+    }
 
     $.widget.bridge(name, $[ namespace ][ name ]);
 
     var plugin = $.fn[ name ];
 
     $.fn[ name ] = function( options ) {
-      if(this.length > 0){
-        var behavior = $[ namespace ][ name ].prototype;
+      $.each(this.selector.split(/,\s?/), function(i, selector) {
+        var behavior = $[ namespace ][ name ].prototype,
+            parts = selector.split(' '),
+            context = parts.length > 1 ? parts[0] : document;
+        selector = (parts.length > 1 ? parts.slice(1) : parts).join(' ');
         $.each(behavior._bindings(), $.proxy(function(i, binding) {
           var handler = function(event) {
             if(!$(this).data(name)) {
@@ -59,18 +82,18 @@
               instance[event.type].call(instance, event, this);
             }
           };
-          if(!binding.target)
-            $(this.selector).live(binding.event, handler);
+          if(!binding.target){
+            $(context).delegate(selector, binding.event, handler);
+          }
         }, this));
-        var returnVal = plugin.call(this, options);
-        $.each(this, function(i, el) {
-          $[ namespace ][ name ].instances.push($(el).data(name));
-        });
-        return returnVal;
-      }
-      else 
-        return this;
+      });
+      var returnVal = plugin.call(this, options);
+      return returnVal;
     };
+    
+    return behavior;
   };
 
 })(jQuery);
+
+
